831137de88bbca237fc3922cd1d56cc6591aad73
Shrikant Kshirsagar <shrikantkshirsagar@macbookpro.mynetworksettings.com>
2025-03-08 16:25:02 -0500

    Remove all old files and folders

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Dashboard() {    
-  return (
-    <>
-      <h1>EcoF:re Dashboard</h1>
-    </>
-  );
-}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>EcoF:re Home page</h1>
-    </>
-  );
-}
